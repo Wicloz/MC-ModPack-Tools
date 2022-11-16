856dc43a83b949b027cb6b1daed7f4f34c3494e8
Wilco de Boer <deboer.wilco@gmail.com>
2022-11-16 16:51:25 +0100

    Merge both tools into single repo

---
+++ export.py
@@ -0,0 +1,187 @@
+import json
+from pathlib import Path
+from tempfile import TemporaryDirectory
+from git import Repo
+from shutil import copyfileobj, copy
+from zipfile import ZipFile
+import requests
+from ruamel.yaml import YAML
+import webbrowser
+from slugify import slugify
+
+if __name__ == '__main__':
+    # load export settings
+    with open('export.yml', 'r') as fp:
+        settings = YAML(typ='safe').load(fp)
+    project_id = str(settings['project'])
+    versions = Path(settings['output'])
+
+    # construct API headers
+    headers = {
+        'X-Api-Token': settings['api-token'],
+        'X-Api-Key': settings['api-key'],
+    }
+
+    # load project and instance data
+    project = requests.get('https://api.curseforge.com/v1/mods/' + project_id, headers=headers).json()['data']
+    with open('minecraftinstance.json', 'r') as fp:
+        instance = json.load(fp)
+    print(json.dumps(instance, indent=2))
+
+    # request version and changelog from user
+    version = input('Enter New Version: ')
+    changelog = input('Enter Changelog: ')
+
+    # determine names for uploads
+    client_pack_name = f'Version {version} Client'
+    server_pack_name = f'Version {version} Server'
+
+    # build initial manifest
+    manifest = {
+        'minecraft': {
+            'version': instance['gameVersion'],
+            'modLoaders': [{
+                'id': instance['baseModLoader']['name'],
+                'primary': True,
+            }],
+        },
+        'manifestType': 'minecraftModpack',
+        'manifestVersion': 1,
+        'name': project['name'],
+        'version': version,
+        'author': project['authors'][0]['name'],
+        'files': [],
+        'overrides': 'overrides',
+    }
+
+    # add managed mods to manifest
+    managed_mods = []
+    for addon in instance['installedAddons']:
+        manifest['files'].append({
+            'projectID': addon['addonID'],
+            'fileID': addon['installedFile']['id'],
+            'required': not addon['installedFile']['FileNameOnDisk'].endswith('.disabled'),
+        })
+        managed_mods.append(addon['installedFile']['FileNameOnDisk'])
+
+    # prepare repository objects
+    repo = Repo()
+    special = {'.gitignore', 'modlist.yml', 'modlist.html'}
+
+    # prepare ZIP outputs
+    if not versions.exists():
+        versions.mkdir()
+    client_zip = versions / (slugify(client_pack_name) + '.zip')
+    server_zip = versions / (slugify(server_pack_name) + '.zip')
+
+    # start building mod pack
+    with TemporaryDirectory() as temp:
+        temp = Path(temp)
+
+        # write manifest file
+        with open(temp / 'manifest.json', 'w') as fp:
+            json.dump(manifest, fp)
+
+        # write changelog file
+        with open(temp / 'modlist.html', 'w') as fp_w:
+            with open('modlist.html', 'r') as fp_r:
+                copyfileobj(fp_r, fp_w)
+
+        # write modlist file
+        with open(temp / 'changelog.txt', 'w') as fp:
+            fp.write(changelog)
+
+        # copy files manged by Git
+        process = list(repo.head.commit.tree)
+        while process:
+            item = process.pop()
+
+            if item.type == 'tree':
+                process += list(item)
+
+            elif item.type == 'blob' and item.name not in special:
+                target = temp / 'overrides' / item.path
+                target.parent.mkdir(parents=True, exist_ok=True)
+                with open(target, 'wb') as fp:
+                    copyfileobj(item.data_stream, fp)
+
+        # create client ZIP from the temp folder
+        with ZipFile(client_zip, 'w') as fp:
+            for file in temp.glob('**/*'):
+                fp.write(file, file.relative_to(temp))
+
+        # copy mods to server folder
+        if not (temp / 'overrides' / 'mods').exists():
+            (temp / 'overrides' / 'mods').mkdir()
+        for file in managed_mods:
+            copy(Path('mods') / file, temp / 'overrides' / 'mods' / file)
+
+        # download Forge installer JAR
+        forge_version = instance['baseModLoader']['minecraftVersion'] + '-' + instance['baseModLoader']['forgeVersion']
+        with open(temp / 'overrides' / f'forge-{forge_version}-installer.jar', 'wb') as fp:
+            fp.write(requests.get(
+                f'https://maven.minecraftforge.net/net/minecraftforge/forge/{forge_version}/forge-{forge_version}-installer.jar'
+            ).content)
+
+        # write server install scripts
+        with open(temp / 'overrides' / 'install.sh', 'w', newline='\n') as fp:
+            fp.write('#!/bin/sh' + '\n')
+            fp.write(f'java -jar "forge-{forge_version}-installer.jar" --installServer' + '\n')
+        with open(temp / 'overrides' / 'install.bat', 'w', newline='\r\n') as fp:
+            fp.write('@ECHO OFF' + '\r\n')
+            fp.write(f'java -jar "forge-{forge_version}-installer.jar" --installServer' + '\r\n')
+
+        # create server ZIP from the overrides folder
+        with ZipFile(server_zip, 'w') as fp:
+            for file in (temp / 'overrides').glob('**/*'):
+                fp.write(file, file.relative_to(temp / 'overrides'))
+
+    # determine game id from version
+    game_ids = []
+    for game in requests.get('https://minecraft.curseforge.com/api/game/versions', headers=headers).json():
+        if game['name'] == instance['gameVersion'] and game['gameVersionTypeID'] == 73407:
+            game_ids.append(game['id'])
+
+    # allow user to check results
+    print()
+    print(f'Please check ZIP files at "{versions.resolve()}"')
+    input('Press Enter to Confirm Upload')
+
+    # upload client pack to CurseForge
+    resp = requests.post(
+        url=f'https://minecraft.curseforge.com/api/projects/{project_id}/upload-file',
+        headers=headers,
+        data={'metadata': json.dumps({
+            'changelog': changelog,
+            'displayName': client_pack_name,
+            'gameVersions': game_ids,
+            'releaseType': 'beta',
+        })},
+        files={'file': open(client_zip, 'rb')},
+    )
+
+    # show upload results
+    if not resp.ok:
+        print(resp.text)
+        exit(1)
+    upload_id = resp.json()['id']
+    webbrowser.open(
+        'https://www.curseforge.com/minecraft/modpacks/' + project['slug'] + '/files/' + str(upload_id)
+    )
+
+    # upload server pack to CurseForge
+    resp = requests.post(
+        url=f'https://minecraft.curseforge.com/api/projects/{project_id}/upload-file',
+        headers=headers,
+        data={'metadata': json.dumps({
+            'changelog': changelog,
+            'displayName': server_pack_name,
+            'parentFileID': upload_id,
+            'additionalFileInfo': 'serverPack',
+            'releaseType': 'beta',
+        })},
+        files={'file': open(server_zip, 'rb')},
+    )
+    if not resp.ok:
+        print(resp.text)
+        exit(1)
---
+++ modlist.py
@@ -0,0 +1,93 @@
+from ruamel.yaml import YAML
+import json
+from pathlib import Path
+import webbrowser
+from jinja2 import Template
+import re
+from urllib.parse import quote
+from subprocess import run, PIPE
+from htmlmin import minify
+
+CACHE = Path(__file__).parent / 'resources' / 'cursecache.json'
+HTML = Path(__file__).parent / 'resources' / 'page.html'
+SCSS = Path(__file__).parent / 'resources' / 'page.scss'
+
+if __name__ == '__main__':
+    with open('modlist.yml', 'r') as fp:
+        definitions = YAML(typ='safe').load(fp)
+        if not definitions:
+            definitions = []
+
+    with open('minecraftinstance.json', 'r') as fp:
+        instance = json.load(fp)
+
+    dependencies = set()
+    files = {}
+    for category in definitions:
+        for pid in category['addons']:
+            for addon in instance['installedAddons']:
+                if addon['addonID'] == pid:
+                    files[pid] = addon['installedFile']['FileNameOnDisk']
+                    dependencies.update(item['addonId'] for item in addon['installedFile']['dependencies'])
+                    break
+            else:
+                files[pid] = ''
+
+    current = set(addon for category in definitions for addon in category['addons'])
+    target = set(addon['addonID'] for addon in instance['installedAddons'])
+
+    print('Addons missing from YML:')
+    for pid in target - current:
+        print('> https://minecraft.curseforge.com/projects/' + str(pid))
+    print()
+
+    if CACHE.exists():
+        with open(CACHE, 'r') as fp:
+            cache = {int(k): v for k, v in json.load(fp).items()}
+    else:
+        cache = {}
+
+    for category in definitions:
+        for pid in category['addons']:
+            if pid not in cache:
+                webbrowser.open('https://minecraft.curseforge.com/projects/' + str(pid))
+                name = input('Paste addon name: ')
+                thumbnail = input('Paste link to thumbnail: ')
+                cache[pid] = {'name': name, 'thumbnail': thumbnail}
+                with open(CACHE, 'w') as fp:
+                    json.dump(cache, fp)
+                print()
+
+    page = []
+    rows = max(category['row'] for category in definitions) + 1
+
+    for i in range(rows):
+        row = []
+
+        for category in definitions:
+            if category['row'] == i:
+                row.append({
+                    'header': category['category'],
+                    'addons': [{
+                        'pid': pid,
+                        'name': re.sub(r'(^|\s)[^\s]+\.[^\s]+(\s|$)', ' ',
+                                       re.sub(r'\(.*?\)', '', cache[pid]['name'])).strip(),
+                        'thumbnail': cache[pid]['thumbnail'],
+                        'missing': files[pid] == '',
+                        'disabled': files[pid].endswith('.disabled'),
+                        'obsolete': category['category'] == 'Libraries' and pid not in dependencies,
+                        'file': files[pid],
+                    } for pid in category['addons']],
+                })
+
+        page.append(row)
+
+    stylesheet = run(('sass.bat', '--style=compressed', SCSS), stdout=PIPE).stdout.decode('UTF8')
+    with open(HTML, 'r') as fp:
+        tm = Template(fp.read())
+
+    with open('modlist.html', 'w') as fp:
+        fp.write(minify(tm.render(page=page, title=Path().resolve().name, stylesheet=stylesheet)))
+
+    print('Generated mod list HTML at:')
+    print('file:///' + quote(str(Path().resolve() / 'modlist.html').replace('\\', '/'), ':/'))
